9f3dc7b619b5e26c4b5f45099e909faf6752d1dc
Ranjitha2493 <54271448+Ranjitha2493@users.noreply.github.com>
2026-05-21 03:04:36 -0500

    Add setup.py for adaptive-astar package
    Created a setup.py file for the adaptive-astar package with metadata and dependencies.

--- setup.py
+++ setup.py
@@ -1,1 +1,25 @@
+cd C:\Users\sbran\Downloads\AdaptiveMazeSolver
 
+# Create setup.py
+@"
+from setuptools import setup, find_packages
+
+setup(
+    name="adaptive-astar",
+    version="1.0.0",
+    description="Adaptive A* Path Planning in Dynamic Environments",
+    author="Ranjitha Shivaprasad Ballakuraya, Hakki Erhan Sevil",
+    author_email="rballakuraya@uwf.edu, hsevil@uwf.edu",
+    url="https://github.com/sevilresearch/AdaptiveMazeSolver",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "pytest",
+    ],
+)
+"@ | Out-File -Encoding UTF8 setup.py
+
+# Verify it was created
+dir setup.py
